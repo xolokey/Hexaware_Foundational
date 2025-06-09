a58c95e7bd1f2c59cdbf11f385427cc7a7c80840
Lokey_Dev <lokeycsi@gmail.com>
2025-06-09 12:50:21 +0530

    Updated

---
+++ MVC/NewWebApplication/Controllers/DepartMentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewWebApplication.Models;
+
+namespace NewWebApplication.Controllers
+{
+    public class DepartMentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Department department = new Department();
+            department.Id = 101;
+            department.Name = "SoftWare Developer";
+            department.Location = "Chennai";
+
+            ViewData["Name"] = "Lokey";
+            ViewData["Domain"] = " .Net FSD";
+            ViewBag.CompanyName = "Hexaware";
+            return View(department);
+        }
+    }
+}
---
+++ MVC/NewWebApplication/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace NewWebApplication.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+    }
+}
