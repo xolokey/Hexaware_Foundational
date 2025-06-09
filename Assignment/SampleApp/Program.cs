25bd30bbaf281ecc6ef9e781b7f1b9a507b461f7
Lokey_Dev <lokeycsi@gmail.com>
2025-06-09 09:35:52 +0530

    Changes Made

--- Assignment/SampleApp/Program.cs
+++ Assignment/SampleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static SampleApp.Sample;
+namespace SampleApp
+{
+    
+    public class Program
+    {
+        
+        static void Main(string[] args) 
+        {
+            Shape shape = new Circle(5);
+            shape.Print();  // Output: This is a shape.
+            double area = shape.GetArea();
+            MathUtils.PrintRounded(area); // Output: Rounded: 79
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Assignment/SampleApp/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace SampleApp
+{
+
+    public class Sample
+    {
+        public abstract class Shape
+        {
+            public abstract double GetArea();
+
+            public void Print()
+            {
+                Console.WriteLine("This is a shape.");
+            }
+        }
+
+        public class Circle : Shape
+        {
+            private double radius;
+
+            public Circle(double r) { radius = r; }
+
+            public override double GetArea() => Math.PI * radius * radius;
+        }
+
+        public static class MathUtils
+        {
+            public static void PrintRounded(double value)
+            {
+                Console.WriteLine("Rounded: " + Math.Round(value));
+            }
+        }
+
+    }
+}
