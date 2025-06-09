c95be992c70fe46b67d0a6abbc3bb0e73797fb97
Lokey_Dev <lokeycsi@gmail.com>
2025-06-09 12:25:25 +0530

    New Web App created

---
+++ MVC/NewWebApplication/Controllers/DepartMentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewWebApplication.Controllers
+{
+    public class DepartMentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Lokey";
+            ViewData["Domain"] = " .Net FSD";
+            ViewBag.CompanyName = "Hexaware";
+            return View();
+        }
+    }
+}
